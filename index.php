ac2dfbb8f53de6d60601a32ac02032d3730f6b89
flaviovitale02 <flaviovitale73@gmail.com>
2023-07-19 19:46:18 +0200

    esercizio

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+    include __DIR__ . '/movie.php';
+
+
+    $film = new movie('The Conjuring - Il caso Enfield', 'HORROR', 'Vera Farmiga', '2016 ');
+
+    echo $film->GetTitle();
+    echo $film->generes;
+    echo '<br>';
+    echo $film->protagonist;
+    echo '<br>';
+    echo $film->date;
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+
+    $film2 = new movie('The Amazing Spider-Man', 'FANTASY', 'Andrew Garfield', '2012 ');
+
+    echo $film2->GetTitle();
+    echo $film2->generes;
+    echo '<br>';
+    echo $film2->protagonist;
+    echo '<br>';
+    echo $film2->date;
+?>
---
+++ movie.php
@@ -0,0 +1,22 @@
+<?php
+        class Movie
+        {
+            public $title;
+            public $generes;
+            public $protagonist;
+            public $date;
+    
+            public function __construct($title, $generes, $protagonist, $date)
+            {
+                $this->title = $title;
+                $this->generes = $generes;
+                $this->protagonist = $protagonist;
+                $this->date = $date;
+            }
+    
+            public function GetTitle()
+            {
+                return $this->title . '<br>';
+            }
+        }
+>
